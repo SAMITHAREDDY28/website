20fed6a79c6282f2fde57fce021257b732bf90c3
SAMITHA REDDY <SAMITHAREDDY28@github.com>
2023-10-04 20:31:33 +0530

    First Commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+var express = require('express');
+var router = express.Router();
+var ctrlmenu = require('../controllers/menu');
+var ctrlcontact = require('../controllers/contact');
+
+/* GET menu page. */
+router.get('/',ctrlmenu.home);
+router.get('/menu',ctrlmenu.menu);
+router.get('/nonveg',ctrlmenu.nonveg);
+router.get('/veg',ctrlmenu.veg);
+router.get('/desert',ctrlmenu.desert);
+/* get contact page */
+router.get('/aboutus',ctrlcontact.aboutus);
+router.get('/contact', ctrlcontact.contact);
+router.get('/reserve', ctrlcontact.reservation);
+module.exports= router;
